fa84867b246fb6e067e84a99013a4f6bccd19e35
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-28 15:04:15 +0000

    Update 5 Assign

---
+++ js/gameScene.js
@@ -0,0 +1,103 @@
+/* global phaser */
+// Created by: Shem
+// Created on: May 2025
+// This is theGame Scene for the game
+
+/**
+ * This class is the Game Scene for the game
+ */
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'gameScene' });
+
+        this.background = null
+        this.ship = null
+        this.fireMissile = false
+    }
+  
+  
+    init(data) {
+        this.cameras.main.setBackgroundColor("AEA04B");
+    }
+  
+    preload() {
+        console.log('Game Scene');
+
+        this.load.image('starBackground', 'assets/starBackground.png')
+        this.load.image('ship', 'assets/spaceShip.png')
+        this.load.image('missile', 'assets/missile.png')
+        // sound
+        this.load.audio('laser', 'assets/laser1.wav')
+    }
+  
+    create(data) {
+        this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+        this.background.setOrigin(0, 0)
+
+        this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+
+        this.missileGroup = this.physics.add.group()
+    }
+  
+    update(time, delta) {
+        
+        const keyLeftObj = this.input.keyboard.addKey('LEFT')
+        const keyRightObj = this.input.keyboard.addKey('RIGHT')
+        const keyUpObj = this.input.keyboard.addKey('UP')
+        const keyDownObj = this.input.keyboard.addKey('DOWN')
+        const keySpaceObj = this.input.keyboard.addKey('SPACE')
+
+
+        if (keyUpObj.isDown === true) {
+            this.ship.y -= 15
+            if (this.ship.y < 0) {
+                this.ship.y = 1080
+            }
+        }
+
+
+
+        if (keyDownObj.isDown === true) {
+            this.ship.y += 15
+            if (this.ship.y > 1080) {
+                this.ship.y = 0
+            }
+        }
+
+
+        if (keyLeftObj.isDown === true) {
+            this.ship.x -= 15
+            if (this.ship.x < 0) {
+                this.ship.x = 1920
+            }
+        }
+        
+        if (keyRightObj.isDown === true) {
+            this.ship.x += 15
+            if (this.ship.x > 1920) {
+                this.ship.x = 0
+            }
+        }
+
+        if (keySpaceObj.isDown === true) {
+            if (this.fireMissile === false) {
+                this.fireMissile = true
+                const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+                this.missileGroup.add(aNewMissile)
+                this.sound.play('laser')
+            }
+        }
+
+        if (keySpaceObj.isUp === true) {
+            this.fireMissile = false
+        }
+
+        this.missileGroup.children.each(function (item) {
+            item.y = item.y - 15
+            if (item.y < 0) {
+                item.destroy()
+            }
+        })
+    }
+}    
+    export default GameScene
